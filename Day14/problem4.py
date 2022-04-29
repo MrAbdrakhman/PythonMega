b6c09587f2737f093eafd37c4a21a6ecb51cea18
MrAbdrakhman <expert-sam@rambler.ru>
2022-04-29 13:13:35 +0600

    update

--- Day14/problem4.py
+++ Day14/problem4.py
@@ -1,5 +1,10 @@
 f = open('python.txt', "w+")
-f.write("#1 Tom grt title hjk asf xc n,. wert")
+f.write('''Python is a widely used high-level programming language for general-purpose
+programming, created by Guido van Rossum and first released in 1991. An interpreted
+language, Python has a design philosophy that emphasizes code readability (notably
+using whitespace indentation to delimit code blocks rather than curly brackets or
+keywords), and a syntax that allows programmers to express concepts in fewer lines of
+code than might be used in languages such as C++ or Java.#1 Tom grt title hjk asf xc n,. wert''')
 f.close()
 with open('python.txt', 'r') as f:
     t_words=[]
