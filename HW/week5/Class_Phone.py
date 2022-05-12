eea262535313726d8a009982bff2d207787fd471
MrAbdrakhman <expert-sam@rambler.ru>
2022-05-13 00:59:12 +0600

    update

--- HW/week5/Class_Phone.py
+++ HW/week5/Class_Phone.py
@@ -7,16 +7,36 @@
 '''
 
 class Phone:
-
     def __init__(self, number, model, weight):
         self.number = number
         self.model = model
         self.weight = weight
 
-    def sebd_message(self):
-        pass
+    def return1(self):
+        return f'{self.number} {self.model} {self.weight}'
+
+    def sendMassage(self):
+        return self.number
+
+    def phone_kays(self):
+        return self.model
+
+    def phone_values(self):
+        return self.number
 
 
-phone = Phone(996441156, "nokia", 45)
-print(dir(phone))
-print(f'номер телефона {phone.number} модель {phone.model} вес {phone.weight} грамм')
+
+samsung = Phone(number=+996705699767, model='Samsung', weight=200)
+print(samsung.return1())
+iphone = Phone(number=+996708699767, model='Iphone', weight=170)
+print(iphone.return1())
+huawei = Phone(number=+996700699767, model='Huawei', weight=250)
+print(huawei.return1())
+print(f'{samsung} \n{iphone} \n{huawei}')
+print(samsung.sendMassage(), iphone.sendMassage(), huawei.sendMassage())
+
+phones = [samsung, iphone, huawei]
+
+print({'Phones': {phone.model: phone.number for phone in phones}})
+
+print(Phone)
