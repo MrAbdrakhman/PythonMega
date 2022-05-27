9aae748697eb39595ab02927ca8661908ba025bf
MrAbdrakhman <expert-sam@rambler.ru>
2022-05-27 23:58:58 +0600

    обновлен

--- HW/week6/Class_Contact_list.py
+++ HW/week6/Class_Contact_list.py
@@ -11,18 +11,36 @@ class List:
         self.id=idn
         self.phone=phone
 
-    def search_by_name(self):
-        pass
+    def search_by_name(self, name):
+        for i in all_contacts:
+            
 
-class ContactList(List):
-    def __init__(self):
-        super(ContactList, self).__init__()
-        pass
+    return (next(x for x in all_contacts if x["name"] == name))
+
+
+    def add_contact(self):
+        all_contacts.append()
+
+# class ContactList(List):
+#     def __init__(self):
+#         super(ContactList, self).__init__()
+#         pass
+    def __repr__(self):
+        return f"id: {self.id}  name: {self.name} surname: {self.surname} phone: {self.phone} "
+
+    def __str__(self):
+        return f"id: {self.id}, name: {self.name}, surname: {self.surname}, phone: {self.phone} "
 
 all_contacts = [ ]
-
-
-
-all_contacts = ContactList()
-
-
+a=List(name="Jhon", surname="Menning", idn=123, phone='15447892')
+all_contacts.append(a)
+b=List("Jake", "Henning", 124, '05447892')
+all_contacts.append(b)
+c=List("Michel", "Obama", 125, '05451456')
+all_contacts.append(c)
+print(all_contacts)
+print(a)
+# search_by_name('Jhon')
+# print(next(x for x in all_contacts if x["name"] == "Jake"))
+# all_contacts = ContactList()
+print([x for x in all_contacts if x['name'] == 'Jake'][1])
